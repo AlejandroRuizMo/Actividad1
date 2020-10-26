ef135fffbec9896bf5458b6fa9d7834134e5adc2
AlejandroRuizMo <73501245+AlejandroRuizMo@users.noreply.github.com>
2020-10-26 16:38:38 -0600

    Update Inicial.py
    Co-Authored-By: lugaso <73501548+lugaso@users.noreply.github.com>

--- Inicial.py
+++ Inicial.py
@@ -3,24 +3,14 @@
 #proposito del programa
 
 #funcion1  calcular área del rectángulo
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 def area(l1,l2):
     riko=l1*l2
     return riko
-=======
->>>>>>> Stashed changes
+
+#funcion2  calcular perímetro del rectángulo
 def Perimetro():
     Per = 2*l1+2*l2
     print("El perimetro de la figura es de", Per)
-
-<<<<<<< Updated upstream
-=======
->>>>>>> 38d54a1eb1c1e29f04d7f270a4b91c71ff783447
->>>>>>> Stashed changes
-#funcion2  calcular perímetro del rectángulo
-
 
 #instrucciones de accion
 #pedir datos
@@ -31,16 +21,9 @@ print("medida de lado 2 del rectángulo")
 l2 = float(input())
 
 #desplegar calculo funcion1
-<<<<<<< Updated upstream
+
 Perimetro()
-#desplegar calculo funcion 2
-=======
-<<<<<<< HEAD
-print(area(l1,l2))
 #desplegar calculo funcion 2
 
-=======
-Perimetro()
-#desplegar calculo funcion 2
->>>>>>> 38d54a1eb1c1e29f04d7f270a4b91c71ff783447
->>>>>>> Stashed changes
+print(area(l1,l2))
+
